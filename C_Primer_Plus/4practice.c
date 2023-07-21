e335a98a7eab2a74e5e1d38fb28a1f3f37330a8d
Teresa <133770977+Teresa-CoCo@users.noreply.github.com>
2023-07-21 08:30:01 +0000

    bug第二版

--- C_Primer_Plus/4practice.c
+++ C_Primer_Plus/4practice.c
@@ -1,48 +1,89 @@
+// #include<stdio.h>
+// #include<unistd.h>
+// int killgram,height;
+// //char name;
+// //int bmi = killgram/(height*height);
+// int main(void)
+
+
+// {
+//     //char name;
+//     //printf("你的名字是什么？\n");
+//     //scanf("%c",&name);
+//    // printf("你好，%c\n",&name);
+
+//    // sleep(3);
+
+//     //int killgram;
+
+
+//     printf("请输入你的体重：\n");
+//     scanf("%d",&killgram);
+
+//     //int height;
+
+//     printf("请输入你的身高：\n");
+//     scanf("%d",&height);
+
+
+//     printf("已经录入，正在计算中，请稍后。\n");
+
+//     float bmi;
+
+//     long doubleheight;
+
+//     doubleheight=height*height;
+
+//     bmi=killgram*doubleheight;
+
+//     printf("您的BMI为%f",&bmi);
+
+//     return 0;
+
+
+
+
+
+
+// }
+
+
+
+//新版本bmi
+
 #include<stdio.h>
-#include<unistd.h>
-int killgram,height;
-char name;
-//int bmi = killgram/(height*height);
-int main(void)
 
 
+int main()
 {
-    //char name;
-    //printf("你的名字是什么？\n");
-    //scanf("%c",&name);
-   // printf("你好，%c\n",&name);
+    int height,weight,yuhu;
+    long bmi;
 
-   // sleep(3);
-
-    //int killgram;
-
-
-    printf("请输入你的体重：\n");
-    scanf("%d",&killgram);
-
-    //int height;
-
-    printf("请输入你的身高：\n");
+    printf("请输入你的身高:\n");
     scanf("%d",&height);
 
+    printf("您的身高为%d",height);
 
-    printf("已经录入，正在计算中，请稍后。\n");
+    printf("请输入体重：\n");
+    scanf("%d",&weight);
 
-    float bmi;
+    printf("您的体重为%d\n",weight);
 
-    long doubleheight;
 
-    doubleheight=height*height;
+    yuhu = height*height;
 
-    bmi=killgram*doubleheight;
+    
 
-    printf("您的BMI为%f",&bmi);
+    //bmi = weight / yuhu; 
+
+    
+
+    
+
+    printf("yuhu为%d\n",yuhu);
+
+    printf("bmi为%d\n",weight%yuhu);
 
     return 0;
-
-
-
-
-
 
 }
