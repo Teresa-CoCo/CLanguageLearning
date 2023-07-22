708eeef8846b5ee12268509284efbf0584c55047
Teresa <primeytklsb@gmail.com>
2023-07-22 22:52:11 +0800

    Fixed bug

--- C_Primer_Plus/bmiplus.c
+++ C_Primer_Plus/bmiplus.c
@@ -1,88 +1,157 @@
+// #include<stdio.h>
+
+// int main(void)
+// {
+//     float height,weight,standardheight,standardweight;
+//     int panduan;
+//     float nuh,yuhu;
+
+//     printf("请输入你的身高:\n");
+//     scanf("%f",&height);
+
+//     if (height>100)
+//     {
+//         standardheight = height / 100;
+//     }
+    
+
+
+//     printf("您的身高为%f米",standardheight);
+
+//     printf("请输入体重：\n");
+    
+//     scanf("%f",&weight);
+
+//     printf("请选择您刚输入体重的单位：1.公斤，2.市斤");
+//     scanf("%d",&panduan);
+
+
+
+//     if (panduan == 2)
+//     {
+//         standardheight = (float)height / 2;
+//     }
+//     else if (panduan == 1)
+//     {
+//         standardheight = (float)weight;
+//     }
+//     else if (panduan != 1 && panduan !=0)
+//     {
+//         printf("您的输入有误,请重新执行。");/* code */
+//         return 0;
+//     }
+    
+    
+
+//     printf("您的体重为%f kg\n",standardweight);
+
+
+//     yuhu = (float)height*height;
+
+    
+
+//     nuh = (float)weight / yuhu; 
+
+    
+
+    
+
+//     printf("yuhu为%.1f\n",yuhu);
+
+//     printf("bmi为%.1f\n",nuh);
+
+//     if(nuh<18.5)
+//     {
+//         printf("体重过轻\n");
+//         return 0;
+//     }
+//     else if(18.5<=nuh && nuh<24)
+//     {
+//         printf("健康体重，请继续保持\n");
+//         return 0;
+
+//     }
+//     else if(24<=nuh && nuh<27)
+//     {
+//         printf("体重过重了\n");
+//         return 0;
+//     }
+//     else if (nuh>=27)
+//     {
+//         printf("体重超重了\n");
+//         return 0;
+//     }
+    
+
+//     return 0;
+
+// }
+
+
+
+
+//Second edition
+
 #include<stdio.h>
 
 int main(void)
 {
-    float height,weight,standardheight,standardweight;
+    float height, weight, standardheight, standardweight;
     int panduan;
-    float nuh,yuhu;
+    float nuh, yuhu;
 
     printf("请输入你的身高:\n");
-    scanf("%f",&height);
+    scanf("%f", &height);
 
-    if (height>100)
-    {
-        standardheight = height / 100;
-    }
-    
+    standardheight = height / 100;
 
-
-    printf("您的身高为%f米",standardheight);
+    printf("您的身高为%f米\n", standardheight);
 
     printf("请输入体重：\n");
-    
-    scanf("%f",&weight);
+    scanf("%f", &weight);
 
-    printf("请选择您刚输入体重的单位：1.公斤，2.市斤");
-    scanf("%d",&panduan);
+    printf("请选择您刚输入体重的单位：1.公斤，2.市斤\n");
+    scanf("%d", &panduan);
 
-
-
-    if (panduan = 2)
+    if (panduan == 2)
     {
-        standardheight = (float)height / 2;
+        standardweight = weight / 2;
     }
-    else if (panduan = 1)
+    else if (panduan == 1)
     {
-        standardheight = (float)weight;
+        standardweight = weight;
     }
-    else if (panduan != 1 && panduan !=0)
+    else
     {
-        printf("您的输入有误,请重新执行。");/* code */
+        printf("您的输入有误，请重新执行。\n");
         return 0;
     }
-    
-    
 
-    printf("您的体重为%f kg\n",standardweight);
+    printf("您的体重为%f kg\n", standardweight);
 
+    yuhu = standardheight * standardheight;
 
-    yuhu = (float)height*height;
+    nuh = standardweight / yuhu;
 
-    
+    printf("yuhu为%.1f\n", yuhu);
+    printf("bmi为%.1f\n", nuh);
 
-    nuh = (float)weight / yuhu; 
-
-    
-
-    
-
-    printf("yuhu为%.1f\n",yuhu);
-
-    printf("bmi为%.1f\n",nuh);
-
-    if(nuh<18.5)
+    if (nuh < 18.5)
     {
         printf("体重过轻\n");
-        return 0;
     }
-    else if(18.5<=nuh && nuh<24)
+    else if (18.5 <= nuh && nuh < 24)
     {
         printf("健康体重，请继续保持\n");
-        return 0;
-
     }
-    else if(24<=nuh && nuh<27)
+    else if (24 <= nuh && nuh < 27)
     {
         printf("体重过重了\n");
-        return 0;
     }
-    else if (nuh>=27)
+    else if (nuh >= 27)
     {
         printf("体重超重了\n");
-        return 0;
     }
-    
 
     return 0;
-
 }
