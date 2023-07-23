6c37c46e81d98c80ee971698bf039ed3d194bef3
Teresa <primeytklsb@gmail.com>
2023-07-23 21:29:42 +0800

    no2+no3 tool loaded

--- C_Primer_Plus/allinone.c
+++ C_Primer_Plus/allinone.c
@@ -84,11 +84,129 @@ int main(void)
     }
     else if(hudshuh == 2)
     {
+        int input;
+        SetConsoleOutputCP(65001);
+        float test,europe,usa,eng,china,temp,linshi;
+        printf("欢迎使用鞋子尺码换算工具");
+        printf("请输入您要使用的模式：\n1.欧洲尺码换算为其他\n2.中国尺码换算为其他尺码\n3.美国尺码换算为其他尺码\n4.英式尺码换算为其他尺码\n（如果不输入默认为欧洲尺码）");
+        scanf("%d",&input);
+        printf("请输入您要换算的尺码：\n");
+        scanf("%f",&test);
+        if (input == 1 || input == 0)//input is europe
+        {
+            china = (test+10)/2 ;
+            europe = test;
+            usa = china -18 +0.5;
+            eng = china -18;
+            printf("经计算，您的中国尺码是%fcm\n您的欧洲尺码是%f码\n您的美国尺码是%f码\n您的英国尺码是%f码\n",china,europe,usa,eng);
+        
+
+        }
+        if (input == 2)//input is china
+        {
+            temp = test;
+        
+            if (temp > 100)  //加入盲输
+            {
+                china = temp / 10;
+            }
+            else
+            {
+                china = temp;
+            }
+        
+            europe = test * 2 - 10;
+            usa = test -18 +0.5;
+            eng = test -18;
+            printf("经计算，您的中国尺码是%fcm\n您的欧洲尺码是%f码\n您的美国尺码是%f码\n您的英国尺码是%f码\n",china,europe,usa,eng);
+
+
+
+        }
+        if (input == 3)//input is usa
+        {
+
+            temp = test + 18 -0.5;
+            china = temp;
+            europe = china * 2 - 10;
+            usa = test ;
+            eng = china -18;
+            printf("经计算，您的中国尺码是%fcm\n您的欧洲尺码是%f码\n您的美国尺码是%f码\n您的英国尺码是%f码\n",china,europe,usa,eng);
+        
+        }
+        if (input == 4)//input is eng
+        {
+
+            temp = test + 18;
+            china = temp;
+        
+            europe = china * 2 - 10;
+            usa = china -18 +0.5;
+            eng = test;
+            printf("经计算，您的中国尺码是%fcm\n您的欧洲尺码是%f码\n您的美国尺码是%f码\n您的英国尺码是%f码\n",china,europe,usa,eng);
+        }
+
+        return 0;
 
     }
     else if(hudshuh == 3)
     {
-        
+
+        float height, weight, standardheight, standardweight;
+        int panduan;
+        float nuh, yuhu;
+        printf("请输入你的身高:\n");
+        scanf("%f", &height);
+
+        standardheight = height / 100;
+
+        printf("您的身高为%f米\n", standardheight);
+
+        printf("请输入体重：\n");
+        scanf("%f", &weight);
+
+        printf("请选择您刚输入体重的单位：1.公斤，2.市斤\n");
+        scanf("%d", &panduan);
+        if (panduan == 2)
+        {
+            standardweight = weight / 2;
+        }
+        else if (panduan == 1)
+        {
+            standardweight = weight;
+        }
+        else
+        {
+            printf("您的输入有误，请重新执行。\n");
+            return 0;
+        }
+
+        printf("您的体重为%f kg\n", standardweight);
+
+        yuhu = standardheight * standardheight;
+
+        nuh = standardweight / yuhu;
+
+        printf("yuhu为%.1f\n", yuhu);
+        printf("bmi为%.1f\n", nuh);
+        if (nuh < 18.5)
+        {
+            printf("体重过轻\n");
+        }
+        else if (18.5 <= nuh && nuh < 24)
+        {
+            printf("健康体重，请继续保持\n");
+        }
+        else if (24 <= nuh && nuh < 27)
+        {
+            printf("体重过重了\n");
+        }
+        else if (nuh >= 27)
+        {
+            printf("体重超重了\n");
+        }
+        return 0;
+
     }
     
 }
