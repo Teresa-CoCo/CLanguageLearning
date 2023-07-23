e2cdd5a75dc616df4b4e8e8bc70bca9b61bcacdf
Teresa <primeytklsb@gmail.com>
2023-07-23 20:35:34 +0800

    全部修改完毕，实现正常功能

--- C_Primer_Plus/shoesize.c
+++ C_Primer_Plus/shoesize.c
@@ -40,20 +40,20 @@ int main(void){
 
     
 
-    if (input == 1 || input == 0)
+    if (input == 1 || input == 0)//input is europe
     {
         
 
 
         china = (test+10)/2 ;
         europe = test;
-        usa = test -18 +0.5;
-        eng = test -18;
+        usa = china -18 +0.5;
+        eng = china -18;
         printf("经计算，您的中国尺码是%fcm\n您的欧洲尺码是%f码\n您的美国尺码是%f码\n您的英国尺码是%f码\n",china,europe,usa,eng);
         
 
     }   
-    if (input == 2)
+    if (input == 2)//input is china
     {
         temp = test;
         
@@ -72,24 +72,24 @@ int main(void){
         printf("经计算，您的中国尺码是%fcm\n您的欧洲尺码是%f码\n您的美国尺码是%f码\n您的英国尺码是%f码\n",china,europe,usa,eng);
 
     }
-    if (input == 3)
+    if (input == 3)//input is usa
     {
         temp = test + 18 -0.5;
         china = temp;
-        europe = test * 2 - 10;
-        usa = test -18 +0.5;
-        eng = test -18;
+        europe = china * 2 - 10;
+        usa = test ;
+        eng = china -18;
         printf("经计算，您的中国尺码是%fcm\n您的欧洲尺码是%f码\n您的美国尺码是%f码\n您的英国尺码是%f码\n",china,europe,usa,eng);
         
     }
-    if (input == 4)
+    if (input == 4)//input is eng
     {
         temp = test + 18;
         china = temp;
         
-        europe = test * 2 - 10;
-        usa = test -18 +0.5;
-        eng = test -18;
+        europe = china * 2 - 10;
+        usa = china -18 +0.5;
+        eng = test;
         printf("经计算，您的中国尺码是%fcm\n您的欧洲尺码是%f码\n您的美国尺码是%f码\n您的英国尺码是%f码\n",china,europe,usa,eng);
     }
     
