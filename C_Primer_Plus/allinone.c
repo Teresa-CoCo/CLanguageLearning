3adc45b05951d44e4f248ead95210581aeef0e09
Teresa <primeytklsb@gmail.com>
2023-07-23 21:16:07 +0800

    allinone version

---
+++ C_Primer_Plus/allinone.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<windows.h>
+
+int main(void)
+{
+    int hudshuh;
+    printf("欢迎使用c语言工具箱V1.0版本\n");
+
+    printf("请输入你想使用的内容：\n1.转换长度工具  2.转换鞋码工具  3.BMI健康指数计算工具\n");
+
+    scanf("%d",hudshuh);
+
+    if (hudshuh == 1)
+    {
+        SetConsoleOutputCP(65001);
+        float chose,input,calculate,mile,foot,meter,inch,yard; //init things
+    }
+    else if(hudshuh == 2)
+    {
+
+    }
+    else if(hudshuh == 3)
+    {
+        
+    }
+    
+}
