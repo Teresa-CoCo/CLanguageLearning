0ec3d3bcfed33f5aea60fb6df9ba7d4a4da19668
Teresa <primeytklsb@gmail.com>
2023-08-02 14:34:26 +0800

    finished edition

--- voidfunctions(12.3).c
+++ voidfunctions(12.3).c
@@ -20,16 +20,58 @@ int main(void)
 {
     printf("欢迎使用万能表\n");
     //int volt,anpei;
+    int dangwei;
+    printf("请选择档位：1.安培档，2.电压档，3.安培档");
+    scanf("%d",&dangwei);
+    if (dangwei == 1)
+    {
+        anpei();
+        
+    }
+    if (dangwei == 2)
+    {
+        volt();
+    }
+    if (dangwei == 3)
+    {
+        omegacal();
+    }
     
+    
+    
+
+
     
 
 }
+void anpei()
+{
+    float volt,anpei,omega;
+    printf("请输入电阻、电压\n");
+    scanf("%d %d",&omega,&volt);
+    anpei == volt / omega;
+    printf("经计算，电流为%f",anpei);
+    return 0;
 
+}
+void volt()
+{
+    float volt,anpei,omega;
+    printf("请输入电阻、电流\n");
+    scanf("%d %d",&omega,&anpei);
+    volt == anpei * omega;
+    printf("经计算，电压为%f",volt);
+    return 0;
+}
 void omegacal()
 {
-    int volt,anpei;
-    printf("请输入电压、电流\n")
-    scanf("%dv%da",volt,anpei);
+    int volt,anpei,omega;
+    printf("请输入电压、电流\n");
+    scanf("%d %d",&volt,&anpei);
+    omega =volt / anpei;
+    printf("经计算，电阻为%f",omega);
+    return 0;
+
 
 
 }
