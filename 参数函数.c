912c83f4e054fa60ccc16973d56630d06df26e27
Teresa <primeytklsb@gmail.com>
2023-08-02 13:59:53 +0800

    basic lines

---
+++ 参数函数.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<windows.h>
+#include<time.h>
+
+void Post();
+
+int main(void)
+{
+    Post();
+    return 0;
+}
+void Post()
+{
+    printf("114514\n");
+    printf("1919810\n");
+    Sleep(5000);
+}
