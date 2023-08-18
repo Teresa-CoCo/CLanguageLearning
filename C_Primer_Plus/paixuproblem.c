83c6323565a997a27bdc80f60f7fd8cc46b2e80a
Teresa-CoCo <primeytklsb@gmail.com>
2023-08-18 22:08:52 +0800

    updated bug version

--- C_Primer_Plus/paixuproblem.c
+++ C_Primer_Plus/paixuproblem.c
@@ -17,6 +17,26 @@ int main()
         array[i]=input;
     }
     //进行排序
-    
-    
+    //int new[total+1];
+    int chche;
+    //先取第二个数(第二个元素是1)
+    for (int i = 1; i <= total; i++)
+    {
+        chche=array[i];
+        for (int j = i-1; j >=0; --j)
+        {
+            if (array[j]<array[i])
+            {
+                array[i]=array[j];
+            }
+            else
+            {
+                break;
+            }
+        
+        }
+        chche=array[i];
+        
+    }
+    return array;
 }
