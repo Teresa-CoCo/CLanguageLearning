a835eb84b2589ac33c307e6da0bf42623e0971a3
Teresa-CoCo <primeytklsb@gmail.com>
2023-10-31 21:08:24 +0800

    Updated ssh function

--- C_Primer_Plus/ssh.h
+++ C_Primer_Plus/ssh.h
@@ -5,15 +5,16 @@ void ssh()
 {
     char username[100];
     int password = 0;
-    printf("请输入你要连接目标计算机的ip地址（默认端口：22，若要更改端口请输入ip:端口）\n");
+    printf("请输入你要连接目标计算机的ip地址:端口,用户名,密码（请使用英文逗号）\n");
     int ssh = 0;
-    scanf("%d",&ssh);
-    printf("请输入你要链接目标计算机作为的用户名");
+    int port = 0;
+    scanf("%d:%d,%s,%s",&ssh,&port,&username,&password);
+    printf("请输入你要链接目标计算机作为的用户名\n");
     scanf("%s",&username);
-    printf("请输入你要链接目标计算机对应用户名的密码");
+    printf("请输入你要链接目标计算机对应用户名的密码\n");
     scanf("%d",&password);
     char code[100];
-    sprintf(code,"ssh %s@%d",username,password);
+    sprintf(code,"ssh %s@%d:%d",username,ssh,port);
 
 
     system(code);
