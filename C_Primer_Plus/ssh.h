490c39f122d06591757ce6e030b089a6b88554c3
Teresa-CoCo <primeytklsb@gmail.com>
2023-10-31 21:12:35 +0800

    improved ssh function(still not work)

--- C_Primer_Plus/ssh.h
+++ C_Primer_Plus/ssh.h
@@ -9,12 +9,13 @@ void ssh()
     int ssh = 0;
     int port = 0;
     scanf("%d:%d,%s,%s",&ssh,&port,&username,&password);
-    printf("请输入你要链接目标计算机作为的用户名\n");
-    scanf("%s",&username);
-    printf("请输入你要链接目标计算机对应用户名的密码\n");
-    scanf("%d",&password);
+    // printf("请输入你要链接目标计算机作为的用户名\n");
+    // scanf("%s",&username);
+    // printf("请输入你要链接目标计算机对应用户名的密码\n");
+    // scanf("%d",&password);
     char code[100];
-    sprintf(code,"ssh %s@%d:%d",username,ssh,port);
+    sprintf(code,"ssh %s@%d:%d",username,ssh,port);//command function error need read ssh function api.doc
+    Sleep(1000);
 
 
     system(code);
